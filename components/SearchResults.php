7185623ab3b4d76fec11afc234da354598b2159c
Pierre-André Vullioud <pa@inetis.ch>
2017-01-10 22:50:26 +0100

    #1 add pagination

--- components/SearchResults.php
+++ components/SearchResults.php
@@ -2,12 +2,15 @@
 
 use Cms\Classes\ComponentBase;
 use Http;
+use Illuminate\Pagination\LengthAwarePaginator;
 use Inetis\GoogleCustomSearch\Models\Settings;
 
 class SearchResults extends ComponentBase
 {
 
     var $search;
+    var $currentPage;
+    var $resultPerPage;
     const APIURL = 'https://www.googleapis.com/customsearch/v1';
 
     public function componentDetails()
@@ -21,6 +24,8 @@ class SearchResults extends ComponentBase
     public function onRun()
     {
         $this->search = get('q');
+        $this->currentPage = (int) get('page', 1);
+        $this->resultPerPage  = (int) Settings::get('resultPerPage', 25);
         $url = $this->buildAPIUrl();
 
         $response = json_decode(Http::get($url));
@@ -29,10 +34,10 @@ class SearchResults extends ComponentBase
         $this->page['totalResults'] = $response->searchInformation->totalResults;
 
         if ($this->page['totalResults']) {
-            $this->page['results'] = $response->items;
+            $this->page['results'] = new LengthAwarePaginator($response->items, $response->searchInformation->totalResults, $this->resultPerPage, $this->currentPage);
+            $this->page['results']->setPath('search');
+            $this->page['results']->addQuery('q', $this->search);
         }
-
-        $resultPerPage  = Settings::get('resultPerPage', 25);
     }
 
     /**
@@ -40,11 +45,11 @@ class SearchResults extends ComponentBase
      */
     private function buildAPIUrl()
     {
-        $start = get('start', 1);
 
         $apiKey         = Settings::get('apikey');
         $cx             = Settings::get('cx');
 
+        $start = ($this->currentPage-1)*$this->resultPerPage+1;
         $params = array('key' => $apiKey,
             'cx' => $cx,
             'start' => $start,
