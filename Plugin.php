0734558bea6745805471d750a3744b5d9728c1b6
Fl0Cri <florent@inetis.ch>
2021-05-27 11:31:13 +0200

    A few code improvements

--- Plugin.php
+++ Plugin.php
@@ -4,21 +4,10 @@ use System\Classes\PluginBase;
 
 class Plugin extends PluginBase
 {
-    public function pluginDetails()
-    {
-        return [
-            'name'        => 'Custom Google Search',
-            'description' => 'Add custom google search results to your site',
-            'author'      => 'inetis',
-            'icon'        => 'icon-search'
-        ];
-    }
-
     public function registerComponents()
     {
         return [
-            '\Inetis\GoogleCustomSearch\Components\SearchResults' => 'searchResults'
-
+            Components\SearchResults::class => 'searchResults',
         ];
     }
 }
